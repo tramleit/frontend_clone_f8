a68e7c50675943372a19af8a9cee2cd19ad1a09d
Mã Việt Hà <mavietha.info@gmail.com>
2022-12-13 15:29:03 +0700

    Build UI sidebar

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -1,5 +1,48 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faNewspaper, faPlus, faRoad, faLightbulb } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <div>Sidebar</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('sidebar')}>
+                <div className={cx('create-blog')}>
+                    <FontAwesomeIcon icon={faPlus} />
+                </div>
+
+                <ul className={cx('list')}>
+                    <li className={cx('item')}>
+                        <Link className={cx('link', 'active')} to="/">
+                            <FontAwesomeIcon icon={faHome} />
+                            <span>Home</span>
+                        </Link>
+                    </li>
+                    <li className={cx('item')}>
+                        <Link className={cx('link')} to="/learning">
+                            <FontAwesomeIcon icon={faRoad} />
+                            <span>Lộ trình</span>
+                        </Link>
+                    </li>
+                    <li className={cx('item')}>
+                        <Link className={cx('link')} to="/courses">
+                            <FontAwesomeIcon icon={faLightbulb} />
+                            <span>Học</span>
+                        </Link>
+                    </li>
+                    <li className={cx('item')}>
+                        <Link className={cx('link')} to="/blog">
+                            <FontAwesomeIcon icon={faNewspaper} />
+                            <span>Blog</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Sidebar;
