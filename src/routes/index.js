96fef94c4ae9f0f07ab77066c543021427af5f91
Mã Việt Hà <mavietha.info@gmail.com>
2022-12-21 00:18:15 +0700

    Build UI Settings

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Blog from '~/pages/Blog';
 import Login from '~/layouts/components/Login';
 import Register from '~/layouts/components/Register';
 import Profile from '~/pages/Profile/Profile';
+import Settings from '~/pages/Settings';
 
 // Public router
 const publicRoutes = [
@@ -15,6 +16,7 @@ const publicRoutes = [
     { path: '/login', component: Login, layout: null },
     { path: '/register', component: Register, layout: null },
     { path: '/@:username', component: Profile },
+    { path: '/settings', component: Settings },
 ];
 
 const privateRoutes = [];
